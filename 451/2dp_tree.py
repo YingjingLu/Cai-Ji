4bceae8019f66b484189a7ecefc21c0c91ea2881
YingjingLu <yingjinglu2019us@gmail.com>
2019-12-17 21:14:46 -0500

    geek

--- 451/2dp_tree.py
+++ 451/2dp_tree.py
@@ -1,5 +1,22 @@
+
+class Binary_Node( object ):
+
+    def __init__( self, weight, left = None, right = None ):
+        self.weight = weight 
+        self.left = left 
+        self.right = right 
+
+class Multi_Node( object ):
+
+    def __init__( self, weight, children ):
+
+        self.weight = weight 
+        self.children = children # node list 
+        
 """
 Max-Weight Indep. Sets on Trees
+
+starting from tree node v the sum of weight of all node is the largest
 """
 
 
---
+++ geek/longest_consecutive_subsequence.py
@@ -0,0 +1,6 @@
+"""
+https://www.geeksforgeeks.org/longest-consecutive-subsequence/
+
+Given an array of integers, find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+"""
+
