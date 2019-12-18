4921e9ed27d7e4bf875b69fe9901dee7c536eb82
YingjingLu <yingjinglu2019us@gmail.com>
2019-12-18 17:19:12 -0500

    2.1.1

--- 451/2dp_tree.py
+++ 451/2dp_tree.py
@@ -1,3 +1,5 @@
+INT_MIN = -2**32
+
 
 class Binary_Node( object ):
 
@@ -17,10 +19,61 @@ class Multi_Node( object ):
 Max-Weight Indep. Sets on Trees
 
 starting from tree node v the sum of weight of all node is the largest
+
+Recursively iterate from child to parents,
+for each node v there are two states:
+    - Take the node 
+    - Not take the node
+
+THe max weight thus defined as the max( U( v ), N( v ) ). 
+Where U( v ) is the weight of taking the current node
+N( v ) is the weight of not taking the current node
+
+If not taking the current node:
+    iterate through its children and find the maximum between:
+    - taking the child 
+    - noe taking the child
+Thus N( v ) = sum_{ u in children of v} max( N( u ), U( u ) )
+
+If taking the current node, then we add the current weight to it:
+U( v ) = w_v + sum_{u \in children of v} N( u )
+
 """
 
+def N( v ):
+    if v.children is None or len( v.children ) == 0:
+        return None 
+    else:
+        _sum = 0
+        for child in v.children:
+            if N( child ) is None:
+                _sum += U( child )
+            else:
+                _sum += max( U( child ), N( child ) )
+        return _sum 
 
+def U( v ):
+    if v.children is None or len( v.children ) == 0:
+        return v.weight 
+    _sum = v.weight 
+    for child in v.children:
+        res = N( child )
+        if res is not None:
+            _sum += res 
+    return _sum 
+
+
+def max_weight_independent_set_on_tree( root ):
+    # base case leaf node: return the weight of the current node
+    if root is None:
+        return 0
+    n = N( root )
+    u = U( root )
+    if n is None:
+        return u
+    return max( u, n )
 
 """
 Optimal Binary Search Trees
 """
+
